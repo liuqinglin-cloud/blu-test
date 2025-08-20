bb85376c31c249477d90d84b1c33ae3736ba5549
刘青林 <liuqinglin@poweroak.net>
2025-08-20 16:31:23 +0800

    增加H5翻译测试，测试H5活动

---
+++ translation_h5/translation_japanese_20250902.py
@@ -0,0 +1,27 @@
+from common_android.assert_methods import *
+from common_android.app_operation import *
+from common_android.basic_operation import click_ele_for_translation
+import unittest
+
+
+class TestEnglish(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.lang = "日语"
+        ST.SAVE_IMAGE = False
+        switch_lang(cls.lang)
+        ST.SAVE_IMAGE = True
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        ST.SAVE_IMAGE = False
+        main_page()
+        ST.SAVE_IMAGE = True
+
+    def tearDown(self):
+        ST.SAVE_IMAGE = False
+        main_page()
+        ST.SAVE_IMAGE = True
