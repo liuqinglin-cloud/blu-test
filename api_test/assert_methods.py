61426728f512a5baaca6e28bb4932fd8bcc893b6
刘青林 <liuqinglin@poweroak.net>
2025-09-05 15:09:30 +0800

    update

---
+++ api_test/assert_methods.py
@@ -0,0 +1,14 @@
+from utils.handle_mysql import *
+from utils.handle_api_data import *
+from utils.handle_redis import *
+from utils.handle_excel import *
+
+def assert_res_data():
+    pass
+
+def assert_sql():
+    pass
+
+def assert_redis():
+    pass
+
