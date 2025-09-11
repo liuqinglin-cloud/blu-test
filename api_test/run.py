959c84e8c8851eea2499acbc669c705f182b0be7
刘青林 <liuqinglin@poweroak.net>
2025-09-11 17:23:39 +0800

    优化

--- api_test/run.py
+++ api_test/run.py
@@ -1,14 +1,13 @@
 import unittest
-import time
 import os.path
-from test_case import TestRunCaseDdt
+from test_case import TestCase
 
 
 case_path = os.path.dirname(__file__)
 project_path = os.path.dirname(os.path.dirname(__file__))
 
 testsuite = unittest.TestSuite()
-#tests = [TestRunCaseDdt('test_marketing')]
+#tests = [TestCase('test_marketing')]
 tests = unittest.TestLoader().discover(case_path,pattern='test_*.py',top_level_dir=None)
 testsuite.addTests(tests)
 testsuite.addTests(tests)
