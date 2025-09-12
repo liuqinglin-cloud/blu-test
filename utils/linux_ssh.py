c6fbc377666a071fe729e5578df1f4e287a514d7
刘青林 <liuqinglin@poweroak.net>
2025-09-12 17:39:25 +0800

    优化

--- utils/linux_ssh.py
+++ utils/linux_ssh.py
@@ -39,20 +39,22 @@ class Ssh:
         """
         stdin, stdout, stderr = self.ssh.exec_command(command)
         result = stdout.read().decode()
+        print(result)
         return result
 
-    def tail_realtime(self, key=None):
+    def tail_realtime(self, key=None, rows=200):
         """
         tail实时查看日志
         :param key: grep关键字，默认None
+        :param rows: 行数，默认100
         """
         transport = self.ssh.get_transport()
         channel = transport.open_session()
         channel.get_pty()
         if key is None:
-            command = f"tail -100f /data/logs/{self.server}/{self.server}.log"
+            command = f"tail -{rows}f /data/logs/{self.server}/{self.server}.log"
         else:
-            command = f"tail -100f /data/logs/{self.server}/{self.server}.log | grep '{key}'"
+            command = f"tail -{rows}f /data/logs/{self.server}/{self.server}.log | grep '{key}'"
         channel.exec_command(command)
         while True:
             if channel.exit_status_ready():
@@ -73,23 +75,31 @@ class Ssh:
         """
         channel = self.ssh.invoke_shell()
         while True:
-            command = input('(输入linux命令)~:')
-            channel.send(command + '\n')
-            time.sleep(1)
-            if channel.recv_ready():
-                output = channel.recv(1024).decode('utf-8')
-                print(output)
+            try:
+                command = input('(输入linux命令)~:')
+                channel.send(command + '\n')
+                time.sleep(1)
+                if channel.recv_ready():
+                    output = channel.recv(1024).decode('utf-8')
+                    print(output)
+            except KeyboardInterrupt:
+                print("Caught control-C")
+                channel.send("\x03")
+                channel.close()
 
-    def cat_grep_log(self, key):
+    def cat_grep_log(self, key, rows=200):
         """
-        grep日志
+        grep日志，执行后close
         :param key: 关键字
+        :param rows: 行数，默认100
         :return: 有日志返回True，else False
         """
-        command = f"cat /data/logs/{self.server}/{self.server}.log | tail -n 100 | grep {key}"
+        command = f"cat /data/logs/{self.server}/{self.server}.log | tail -n {rows} | grep {key}"
         result = self.execute_channel(command)
         return True if result else False
 
 
 if __name__ == '__main__':
+    ssh = Ssh("营销")
+    ssh.tail_realtime()
     pass
