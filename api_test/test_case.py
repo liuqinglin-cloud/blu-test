f29f0d519a0ac94c901ef81681db4ed448020a32
刘青林 <liuqinglin@poweroak.net>
2025-09-12 10:11:33 +0800

    优化

--- api_test/test_case.py
+++ api_test/test_case.py
@@ -4,15 +4,15 @@ from api_request import run_request
 from utils.handle_excel import case_data
 
 marketing,FAQ = 0,1
-test_marketing_data = case_data.get_excel_data(marketing)
+test_blu_marketing_center_data = case_data.get_excel_data(marketing)
 test_FAQ_data = case_data.get_excel_data(FAQ)
 
 
 @ddt.ddt
 class TestCase(unittest.TestCase):
 
-    @ddt.data(*test_marketing_data)
-    def test_marketing(self, data):
+    @ddt.data(*test_blu_marketing_center_data)
+    def test_blu_marketing_center(self, data):
         run_request(data,marketing)
 
     @ddt.data(*test_FAQ_data)
